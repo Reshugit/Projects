bf226183415fc4867957dc783b3d916b2d546e45
Reshugit <131754527+Reshugit@users.noreply.github.com>
2023-04-26 20:57:55 +0530

    Update main.py

--- Mycodes/main.py
+++ Mycodes/main.py
@@ -32,11 +32,9 @@ if user_choice >= 3 or user_choice < 0:
     print("You typed an invalid number,you lose!")
 else:
     print(game_images[user_choice])
-
     computer_choice = random.randint(0, 2)
     print("computer chose:")
     print(game_images[computer_choice])
-
     if user_choice == 0 and computer_choice == 2:
         print("You win!")
     elif computer_choice == 0 and user_choice == 2:
